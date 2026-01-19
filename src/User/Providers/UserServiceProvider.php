7d177061c0a006330288f33f06f3cb9ae2475aeb
Y-ostin <yostin.arequipa@tecsup.edu.pe>
2026-01-19 13:05:43 -0500

    feat: clean routes

--- src/User/Providers/UserServiceProvider.php
+++ src/User/Providers/UserServiceProvider.php
@@ -6,7 +6,6 @@ use Illuminate\Support\ServiceProvider;
 use Src\User\Application\Services\UserService;
 use Src\User\Domain\Repositories\UserRepository;
 use Src\User\Domain\Rules\UserRules;
-use Src\User\Infrastructure\Persistence\EloquentUserRepository;
 
 
 class UserServiceProvider extends ServiceProvider
@@ -25,18 +24,8 @@ class UserServiceProvider extends ServiceProvider
         $this->app->singleton(UserService::class, function ($app) {
             return new UserService(
                 $app->make(UserRepository::class),
-                $app->make(UserRules::class) // <--- AGREGA ESTA LÍNEA
+                $app->make(UserRules::class) 
             );
         });
-    }
-
-    public function boot(): void
-    {
-        
-        $routesPath = base_path('src/User/Infrastructure/Routes/api.php');
-        
-        if (file_exists($routesPath)) {
-            $this->loadRoutesFrom($routesPath);
-        }
     }
 }
