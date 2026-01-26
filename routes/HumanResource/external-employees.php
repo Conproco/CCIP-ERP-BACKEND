87cfcba606a323b36a27cacbc48cfdabfc1a4258
JonathanFPJV <jonathan.juli@tecsup.edu.pe>
2026-01-26 10:03:01 -0500

    refactor(routes): standardize external employee route names

--- routes/HumanResource/external-employees.php
+++ routes/HumanResource/external-employees.php
@@ -13,18 +13,18 @@ use App\Http\Controllers\HumanResource\ExternalEmployeesController;
 
 Route::prefix('human-resource/external-employees')->group(function () {
     // Index - Obtener datos iniciales (líneas de costo)
-    Route::get('/', [ExternalEmployeesController::class, 'index'])->name('hr.external-employees.index');
+    Route::get('/', [ExternalEmployeesController::class, 'index'])->name('employees.external.index');
 
     // Lista paginada de empleados externos con filtros
-    Route::get('/list', [ExternalEmployeesController::class, 'getExternalEmployees'])->name('hr.external-employees.list');
+    Route::get('/list', [ExternalEmployeesController::class, 'getExternalEmployees'])->name('employees.external.getExternalEmployee');
 
     // CRUD
-    Route::post('/', [ExternalEmployeesController::class, 'createExternalEmployee'])->name('hr.external-employees.store');
-    Route::put('/{external_id}', [ExternalEmployeesController::class, 'updateExternalEmployee'])->name('hr.external-employees.update')->whereNumber('external_id');
-    Route::delete('/{external_id}', [ExternalEmployeesController::class, 'deleteExternalEmployee'])->name('hr.external-employees.destroy')->whereNumber('external_id');
+    Route::post('/', [ExternalEmployeesController::class, 'createExternalEmployee'])->name('employees.external.create');
+    Route::put('/{external_id}', [ExternalEmployeesController::class, 'updateExternalEmployee'])->name('employees.external.update')->whereNumber('external_id');
+    Route::delete('/{external_id}', [ExternalEmployeesController::class, 'deleteExternalEmployee'])->name('employees.external.delete')->whereNumber('external_id');
 
     // Archivos
-    Route::get('/{id}/profile-image', [ExternalEmployeesController::class, 'showExternalProfileImage'])->name('hr.external-employees.profile-image')->whereNumber('id');
-    Route::get('/{id}/curriculum-vitae', [ExternalEmployeesController::class, 'previewCurriculumVitae'])->name('hr.external-employees.curriculum-vitae')->whereNumber('id');
+    Route::get('/{id}/profile-image', [ExternalEmployeesController::class, 'showExternalProfileImage'])->name('management.external.employee.profile.show')->whereNumber('id');
+    Route::get('/{id}/curriculum-vitae', [ExternalEmployeesController::class, 'previewCurriculumVitae'])->name('employees.external.preview.curriculum_vitae')->whereNumber('id');
     
 });
